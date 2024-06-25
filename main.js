da5195d5d859ba576f99b5baf15f2e87011737a2
gaurav bisht <gauravengg.bisht@gmail.com>
2024-06-25 22:31:10 +0530

    basic operation is done

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-const goal_container = document.querySelectorAll('.goal_container');
-const custom_checkbox = document.querySelectorAll('.custom_checkbox');
+const input_feilds = document.querySelectorAll('.goal_input');
+const custom_checkboxlist = document.querySelectorAll('.custom_checkbox');
+const appContainer = document.querySelector('.app_container');
+const error_label = document.querySelector('.error_label');
+const progress_value = document.querySelector('.progress_value');
 
-console.log(custom_checkbox)
+custom_checkboxlist.forEach(element => {
+    element.addEventListener('click',(e)=>{
+        const allinputFeilds = [...input_feilds].every((input)=>{
+            return input.value;
+        })
+        if(allinputFeilds){
+            element.parentElement.classList.toggle('completed');
+            progress_value.style.width = '33.33%'
+        }
+        else{
+            appContainer.classList.add('showError')
+        }
+    })
+});
+
+input_feilds.forEach((input)=>{
+    input.addEventListener('focus',()=>{
+        appContainer.classList.remove('showError')
+    })
+})
